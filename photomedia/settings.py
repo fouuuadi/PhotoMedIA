0388dcd5375560125c1ab077a7b84e1f03e3be4b
fouuuadi <fouadlamnaouar18@gmail.com>
2025-04-30 15:16:56 +0200

    setup supabase

--- photomedia/settings.py
+++ photomedia/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
